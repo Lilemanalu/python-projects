06a448788212a98eea02a87fcf4ff67e5e341a1a
Lile <70990797+Lilemanalu12S20017@users.noreply.github.com>
2023-04-09 20:37:34 +0700

    blind auction

---
+++ blind-auction/main.py
@@ -0,0 +1,37 @@
+from os import system, name
+from art import logo
+
+def clear():
+    # For Windows
+    if name == 'nt':
+        _ = system('cls')
+    # For Mac and Linux
+    else:
+        _ = system('clear')
+
+print(logo)
+
+bids = {}
+bidding_continue = True
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid: 
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while bidding_continue:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        bidding_continue = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        clear()
+
